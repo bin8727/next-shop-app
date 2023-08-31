46c82ade6cc12c11ba2c87ad124f091e6676302d
MoYuBin <bin8727@naver.com>
2023-08-31 12:17:27 +0900

    feat #11: 홈 화면 슬라이더 구현

---
+++ src/components/slider/SliderData.js
@@ -0,0 +1,19 @@
+const sliderData = [
+  {
+    image: 'https://images.unsplash.com/photo-1693376753872-6d5bc01b3109?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80',
+    heading: 'title 1',
+    desc: 'description 1',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1608539092555-f539fd0e5c87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3174&q=80',
+    heading: 'title 2',
+    desc: 'description 2',
+  },
+  {
+    image: 'https://plus.unsplash.com/premium_photo-1671070687996-5b1485638342?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3173&q=80',
+    heading: 'title 3',
+    desc: 'description 3',
+  },
+];
+
+export default sliderData;
